cae8ad7dc4ed6d3e60e1f7e7ff3d972cca640616
Adriano <adrianotiger@hotmail.com>
2016-01-26 15:41:43 +0100

    Resources
    Adding resources and gitignore

--- AboutBox1.Designer.cs
+++ AboutBox1.Designer.cs
@@ -69,7 +69,6 @@
             // 
             this.logoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.logoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.Image")));
-            this.logoPictureBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.InitialImage")));
             this.logoPictureBox.Location = new System.Drawing.Point(3, 3);
             this.logoPictureBox.Name = "logoPictureBox";
             this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 6);
